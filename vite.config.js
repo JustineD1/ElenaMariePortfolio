aee80d71cfcb586565a566cca99509000bf85d39
Justine De Juan <132037584+JustineD1@users.noreply.github.com>
2026-07-14 18:29:55 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/ElenaMariePortfolio/' // Add this exact line
+  base: isProd ? '/ElenaMariePortfolio/' : '/'
 })
