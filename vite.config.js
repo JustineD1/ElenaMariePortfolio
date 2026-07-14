026ab7e7282ab52b5465cd9e11c43379c2f59c92
Justine De Juan <132037584+JustineD1@users.noreply.github.com>
2026-07-14 19:03:21 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => ({
   plugins: [vue(), tailwindcss()],
-  base: mode === 'production' ? '/ElenaMariePortfolio/' : '/',
+  base: mode === 'production' ? './' : '/',
 }))
